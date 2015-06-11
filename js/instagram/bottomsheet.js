68f30abfc1961e1ec0fcd6b65e446696470a9583
rrlara <rrlara@gmail.com>
2015-06-10 19:13:31 -0700

    Added image dialog

--- js/instagram/bottomsheet.js
+++ js/instagram/bottomsheet.js
@@ -4,7 +4,7 @@ app.controller('BottomSheetCtrl', function($scope, $rootScope, Instagram) {
 
 	var googleStaticImage = 'http://maps.googleapis.com/maps/api/staticmap?' +
 		'center=40.714728,-73.998672&zoom=12&scale=2&size=600x300&maptype=roadmap&format=png&visual_refresh=true&' +
-		'markers=size:mid%7Ccolor:0x053dff%7Clabel:0%7CAlbany,+NY',
+		'markers=size:mid%7Ccolor:0x053dff%7Clabel:0%7CAlbany,+NY';
 
 	
 });
