a832edf8fdfca0a6b6c754dc84b7c02afa7f9cb3
rrlara <rrlara@gmail.com>
2015-06-12 15:07:04 -0500

    added captions to dialogs

--- js/instagram/bottomsheet.js
+++ js/instagram/bottomsheet.js
@@ -6,7 +6,7 @@ app.controller('BottomSheetCtrl', function($scope, $rootScope) {
 	var	lng = $rootScope.location.lng;
 
 	$scope.Image = 'https://maps.googleapis.com/maps/api/staticmap?scale=1' +
-		'&zoom=13&size=640x640&sensor=false&maptype=terrain&markers=color:red|' + lat + ',' + lng;
+		'&zoom=15&size=640x640&sensor=false&maptype=terrain&markers=color:red|' + lat + ',' + lng;
 
 	console.log($scope.Image);
 
