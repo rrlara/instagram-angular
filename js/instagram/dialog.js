f03983fc5f00840b33637b75de63d5f96c6eb48d
rrlara <rrlara@gmail.com>
2015-06-24 19:12:21 -0700

    changes

--- js/instagram/dialog.js
+++ js/instagram/dialog.js
@@ -7,6 +7,8 @@ app.controller('DialogCtrl', function($scope, $rootScope) {
 	$scope.isImage = false;
 	$scope.isVideo = false;
 
+	console.log("$rootScope.imageClicked", $rootScope.imageClicked);
+
 	if ($rootScope.imageClicked[0].type == 'video'){
 		$scope.isVideo = true;
 		$scope.Image = $rootScope.imageClicked[0].videos.standard_resolution.url;
@@ -20,6 +22,10 @@ app.controller('DialogCtrl', function($scope, $rootScope) {
 	$scope.userName = $rootScope.imageClicked[0].user.full_name;
 
 	$scope.userNameLink = "https://instagram.com/" + $rootScope.imageClicked[0].user.username + "/";
+
+	$scope.likeCount = $rootScope.imageClicked[0].likes.count;
+
+	$scope.commentCount = $rootScope.imageClicked[0].comments.count;
 
 
 
